79ed870365c16378d22f0f40ad76b7882b76add2
Olivia Owen <olivia.owen@hotmail.com>
2021-03-27 18:39:05 +0000

    completed comment section and form

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,14 +1,14 @@
 async function commentFormHandler(event) {
     event.preventDefault();
   
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+    const comment_text = document.querySelector('#comment-body').value.trim();
   
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
   
     if (comment_text) {
-        const response = await fetch('/api/comments', {
+        const response = await fetch('/post/comments', {
           method: 'POST',
           body: JSON.stringify({
             post_id,
@@ -26,4 +26,8 @@ async function commentFormHandler(event) {
         }
       }
   }
+
+  document
+    .querySelector('.comment-post-form')
+    .addEventListener('submit', commentFormHandler);
   
