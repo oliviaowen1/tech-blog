a7ffa8224ed4ad7f1634d7925508d31877b28690
Olivia Owen <olivia.owen@hotmail.com>
2021-03-28 15:02:49 +0100

    updated models

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment')
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -9,6 +9,24 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+// Comment to User associations - users have many comments and a comment belongs to a user
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Comment to Post associations - blog postss have many comments - a comment belongs to a blog posts
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
